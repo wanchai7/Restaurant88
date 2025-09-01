524133577a429037da63a307369b78dcd6eebe05
Wanchai7 <wanchai.view47@gmail.com>
2025-09-01 14:19:40 +0700

    sercer

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const app = express();
 const dotenv = require("dotenv");
 dotenv.config();
 const PORT = process.env.PORT || 5000;
+const FRONTEND_URL = process.env.FRONTEND_URL;
 const restaurantRouter = require("./routers/restaurant.router");
 const cors = require("cors");
 
